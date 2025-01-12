55d370a889bc9d1f6464e8e78e7ac55fe84a292e
Estebra <c.esteban.hernandezbr@gmail.com>
2025-01-12 17:25:50 -0600

    Completed the swap cases script

--- swap_cases.py
+++ swap_cases.py
@@ -1,8 +1,24 @@
+# Function to swap the letters
 def swap_case(s):
+    # list to add the converted
+    new_list = []
+    # make the string into a list to iterate
+    letters = list(s)
+    # abc = 'abcdefghijklmnopqrstuvwxyz' # use in previous solution
 
-    return
+    for char in letters:
+        # if char in abc:
+        if char.islower():
+            new_list.append(char.upper())
+        # elif char in abc.upper():
+        elif char.isupper():
+            new_list.append(char.lower())
+        else:
+            new_list.append(char)
 
-if __name__ = '__main__':
+    return ''.join(new_list)
+
+if __name__ == '__main__':
     s = input()
     result = swap_case(s)
     print(result)
