a99bd6c343291ebb9c4a2176e78ee3f027beb351
Estebra <c.esteban.hernandezbr@gmail.com>
2025-01-12 16:48:37 -0600

    finish list manipulation script and start swap cases script

--- list_manipulation.py
+++ list_manipulation.py
@@ -1,24 +1,26 @@
-arr = []
-def list_manipulation(action, arr, e=None, i=None):
-    match action:
-        case 'insert':
-            arr.insert(i, e)
-        case 'print':
-            print(arr)
-        case 'remove':
-            arr.remove(e)
-        case 'append':
-            arr.append(e)
-        case 'sort':
-            arr.sort()
-        case 'pop':
-            arr.pop()
-        case 'reverse':
-            arr.reverse()
-        case _:
-            print('Than option is not supported!')
+def perform_list_operations(n):
+    my_list = []
+
+    for _ in range(n):
+        command = input().split()
+
+        match command[0]:
+            case 'insert':
+                my_list.insert(int(command[1]), int(command[2]))
+            case 'print':
+                print(my_list)
+            case 'remove':
+                my_list.remove(int(command[1]))
+            case 'append':
+                my_list.append(int(command[1]))
+            case 'sort':
+                my_list.sort()
+            case 'pop':
+                my_list.pop()
+            case 'reverse':
+                my_list.reverse()
+
 
 if __name__ == '__main__':
     N = int(input())
-    for i in range(N):
-        list_manipulation(input(), arr)
+    perform_list_operations(N)
